d10d1ae49285e53d1655f4ceb59743540f148f29
Abhijit Gaikwad <agaikwad@marketlive.com>
2014-03-27 03:33:47 -0700

    post

--- js/controllers/PetDetailCtrl.js
+++ js/controllers/PetDetailCtrl.js
@@ -1,24 +1,40 @@
 // A simple controller that shows a tapped item's data
-angular.module('starter.controllers').controller('CatchAllController', function($scope,$http,
-    $stateParams, PetService) {
+angular.module('starter.controllers').controller('CatchAllController', function($scope, $http,
+    $stateParams) {
+   
+$scope.user={name:"",email:"",gender:""};
+ $scope.update = function(user) {
+console.log(user);
+};
 
 
-    $scope.sendEmail = function() {
-        $http({
-            method: 'post',
-            url: 'http://gabhi.com/feedback.php',
-            data: {
-                'email': $scope.email,
-                'name': $scope.name,
-                'feedback': $scope.feedback
+    $scope.sendEmail = function(user) {
+        $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+        console.log("ddddd");
+        console.log(user.name+user.email+user.feedback);
+        $http.post(
+            
+           'http://gabhi.com/feedback.php',
+            {
+                'email': user.email,
+                'name': user.name,
+                'feedback': user.feedback
 
             }
-        }).
+        ).
         success(function(data, status) {
+          //  alert("Thank you");
+          console.log(data);
+            $scope.user={name:"",email:"",gender:""};
 
         }).
         error(function(data, status) {
+            alert(status);
+            $scope.user={name:"",email:"",gender:""};
+
 
         });
+
+
     };
 });
